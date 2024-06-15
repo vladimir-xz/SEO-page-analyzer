0a57b58ed6b9333cd2f27ace586c7d699c0872d1
vladimir-xz <vladimir156xz@gmail.com>
2024-06-15 12:33:52 +0200

    working processor slim and first page

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Middleware\MethodOverrideMiddleware;
 use Slim\Factory\AppFactory;
 use DI\Container;
 
@@ -9,14 +10,17 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $container = new Container();
 $container->set('renderer', function () {
-    // Параметром передается базовая директория, в которой будут храниться шаблоны
     return new \Slim\Views\PhpRenderer(__DIR__ . '/../view');
 });
 
-$app = AppFactory::create();
+$app = AppFactory::createFromContainer($container);
+$app->addErrorMiddleware(true, true, true);
+$app->add(MethodOverrideMiddleware::class);
+$router = $app->getRouteCollector()->getRouteParser();
 
 $app->get('/', function (Request $request, Response $response) {
-    return $this->get('renderer')->renderer('response', 'index.phtml');
+    return $this->get('renderer')->render($response, 'index.phtml');
 });
+
 
 $app->run();
