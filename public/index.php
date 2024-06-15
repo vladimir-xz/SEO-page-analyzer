730bcd598fcc10104fff31b3416c59965d0adb51
vladimir-xz <vladimir156xz@gmail.com>
2024-06-15 12:13:07 +0200

    index.phtml and slim processor

--- public/index.php
+++ public/index.php
@@ -3,14 +3,20 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use DI\Container;
 
 require __DIR__ . '/../vendor/autoload.php';
+
+$container = new Container();
+$container->set('renderer', function () {
+    // Параметром передается базовая директория, в которой будут храниться шаблоны
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../view');
+});
 
 $app = AppFactory::create();
 
 $app->get('/', function (Request $request, Response $response) {
-    $response->getBody()->write("Welcome to my third project!");
-    return $response;
+    return $this->get('renderer')->renderer('response', 'index.phtml');
 });
 
 $app->run();
