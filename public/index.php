dd2744b54fc3a6aa23bc62f7b549251f28d564c4
Vladimir <vladimir156xz@gmail.com>
2024-06-17 03:51:34 +0200

    error and success messages

--- public/index.php
+++ public/index.php
@@ -8,9 +8,14 @@ use Hexlet\Code\Validator;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+session_start();
+
 $container = new Container();
 $container->set('renderer', function () {
     return new \Slim\Views\PhpRenderer(__DIR__ . '/../view');
+});
+$container->set('flash', function () {
+    return new \Slim\Flash\Messages();
 });
 
 $app = AppFactory::createFromContainer($container);
@@ -19,15 +24,18 @@ $app->add(MethodOverrideMiddleware::class);
 $router = $app->getRouteCollector()->getRouteParser();
 
 $app->get('/', function ($request, $response) {
+    $messages = $this->get('flash')->getMessages();
     return $this->get('renderer')->render($response, 'index.phtml');
 });
 
 $app->get('/urls/{id}', function ($request, $response, $args) {
+    $messages = $this->get('flash')->getMessages();
     $id = $args['id'];
     $engine = new Engine('urls', 'find', $id);
     $url = $engine->process();
     $params = [
         'url' => $url,
+        'flash' => $messages
     ];
     return $this->get('renderer')->render($response, 'url.phtml', $params);
 });
@@ -45,12 +53,15 @@ $app->post('/urls', function ($request, $response) {
     $validator = new Validator();
     $url = $request->getParsedBodyParam('url');
     $errors = $validator->validate($url);
+    if (count($errors) === 0) {
         $engine = new Engine('urls', 'insert', $url['name']);
         $insertedId = $engine->process();
+        $this->get('flash')->addMessage('success', 'Страница успешно добавлена');
         return $response->withRedirect("/urls/{$insertedId}", 302);
+    }
     $params = [
         'url' => $url,
-        'errors' => $errors
+        'errors' => 5
     ];
     return $this->get('renderer')->render($response, "index.phtml", $params);
 });
--- src/Validator.php
+++ src/Validator.php
@@ -7,6 +7,7 @@ class Validator
     public function validate(array $url): array
     {
         $errors = [];
+        return [];
         if (strlen($url['name']) > 255) {
             $errors['strlen'] = "Url must be less than 256 characters";
         } elseif (!filter_var($url['name'], FILTER_VALIDATE_URL)) {
